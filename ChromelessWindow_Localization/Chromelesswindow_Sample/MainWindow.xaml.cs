dd21814249956e3850eb5b707833d38291c0f873
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-11-02 10:23:46 +0530

    Added sample for ChromelessWindow localization

---
+++ ChromelessWindow_Localization/Chromelesswindow_Sample/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chromelesswindow_Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ChromelessWindow
+    {
+        public MainWindow()
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("de-DE");
+            InitializeComponent();
+        }
+    }
+}
